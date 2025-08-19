50f51e49652e4a3512cf738393171a0e3a2141d0
yukesh03n <yukesh03n@gmail.com>
2025-08-20 00:30:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const mongoSanitize = require("express-mongo-sanitize");
 const path = require('path');
 const clientBuildPath = path.join(__dirname, "../client/build");
+const cors = require('cors');
 
 const app = express();
 app.use(helmet());
